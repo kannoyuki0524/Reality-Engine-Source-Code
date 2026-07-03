75fcbe1f924da7ae935daa9157eb3c0adf6e55ad
Lv233 <3506095220@qq.com>
2026-07-03 23:44:42 +0800

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -18,6 +18,7 @@ import lime.utils.Assets;
 import flixel.system.FlxSound;
 import openfl.utils.Assets as OpenFlAssets;
 import WeekData;
+import ErrorState;
 #if MODS_ALLOWED
 import sys.FileSystem;
 #end
